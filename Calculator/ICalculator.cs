6bd9901e394c46a3f337c1526f045225248f2d47
Danila <danilamatush72@gmail.com>
2025-10-07 11:43:23 +0300

    add DefineOperation and IsSupportedOperation to ICalculator

--- Calculator/ICalculator.cs
+++ Calculator/ICalculator.cs
@@ -1,6 +1,10 @@
+using System.Linq.Expressions;
+
 namespace Calculator;
 
 public interface ICalculator
 {
     public double Calculate(double operand1, double operand2, Func<double, double, double> operation);
+    public Func<double, double, double> DefineOperation(string operation);
+    public bool IsSupportedOperation(string operation);
 }
