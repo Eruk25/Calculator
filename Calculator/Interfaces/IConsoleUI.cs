813880ccaa47511d6f6adb4a1e0fde7e5b0b5ccc
Danila <danilamatush72@gmail.com>
2025-10-07 16:47:49 +0300

    feat(calculator): add IConsoleUI interface

--- Calculator/Interfaces/IConsoleUI.cs
+++ Calculator/Interfaces/IConsoleUI.cs
@@ -1,6 +1,10 @@
 namespace Calculator;
 
-public interface IConsoleUI
+public interface IConsoleUi
 {
-    
+    public double ReadOperand(string prompt);
+    public void Write(string prompt);
+    public void Run();
+    public string ReadOperation(string prompt);
+    public bool AskToContinue();
 }
